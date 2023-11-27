92426c1e27d545e91b43ae607c7125f53fa1f8bb
paradox8599 <paradox8599@outlook.com>
2023-11-27 11:09:44 +1100

    add theme token ref url

--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -1,5 +1,7 @@
 import type { ThemeConfig } from "antd";
 
+// check the following url for theme tokens
+// https://ant.design/docs/react/customize-theme#seedtoken
 const theme: ThemeConfig = {
   token: {},
 };
