378bb5989e5e6c41a2243be3436e2e66c44b5ba9
Rajib-Guha <148442485+Rajib-Guha@users.noreply.github.com>
2023-11-27 12:12:19 +1100

    changed button and added labels (#7)

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Button } from "antd";
 import { Formik, Form, Field } from "formik";
 
 export default function SignInPage() {
@@ -11,7 +12,7 @@ export default function SignInPage() {
           "url('https://media.istockphoto.com/id/1077026512/photo/nature-inspired-table.jpg?s=612x612&w=0&k=20&c=TthBCamXFsPZt2CaCfd2EzuzeImWiwKTUZJTMEIKFcI=')",
       }}
     >
-      <div className="bg-white bg-opacity-90 px-12 py-6 rounded-lg shadow-md relative">
+      <div className="bg-white bg-opacity-90 px-12 py-6 rounded-lg shadow-md relative sm:mx-4">
         <div className="h-24 w-5/6 bg-cyan-950 absolute -top-4 left-1/2 transform -translate-x-1/2 rounded-lg flex items-center justify-center">
           <p className="text-white font-mono font-semibold tracking-widest text-4xl">
             Vendure
@@ -26,28 +27,32 @@ export default function SignInPage() {
         >
           <Form className="w-full max-w-xl space-y-4 mt-24">
             <div className="flex flex-col">
+              <label htmlFor="username" className="text-gray-700 font-semibold mb-1">Username or Email:</label>
               <Field
                 type="text"
+                id="username"
                 name="username"
-                placeholder="Username or email"
                 className="border border-gray-300 rounded-md py-2 px-6 w-full"
               />
             </div>
             <div className="flex flex-col pb-5">
+              <label htmlFor="password" className="text-gray-700 font-semibold mb-1">Password:</label>
               <Field
                 type="password"
+                id="password"
                 name="password"
-                placeholder="Password"
                 className="border border-gray-300 rounded-md py-2 px-6 w-full"
               />
             </div>
 
-            <button
-              type="submit"
-              className="w-full bg-cyan-950 text-white rounded-md py-2 mt-2 hover:bg-blue-600 transition duration-300"
+
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="w-full"
             >
               Sign In
-            </button>
+            </Button>
 
             <div className="text-center pt-2">
               <a href="#" className="text-sm text-amber-900">
