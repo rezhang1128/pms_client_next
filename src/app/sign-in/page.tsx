10c3ae4b6a5e07fb1a653f349080085be2a2274e
Rajib-Guha <148442485+Rajib-Guha@users.noreply.github.com>
2023-12-03 22:55:24 +1100

    [Signin] Added Link tags and improved responsiveness (#8)
    * Replaced 'a' links with Link component, removed flex flex-col properties
    
    * Added Links and improved responsiveness

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -2,18 +2,17 @@
 
 import { Button } from "antd";
 import { Formik, Form, Field } from "formik";
+import Link from 'next/link';
 
 export default function SignInPage() {
   return (
-    <div
-      className="min-h-screen flex flex-col items-center justify-center bg-cover bg-center"
+    <div className="min-h-screen flex flex-col items-center justify-center bg-cover bg-center"
       style={{
         backgroundImage:
           "url('https://media.istockphoto.com/id/1077026512/photo/nature-inspired-table.jpg?s=612x612&w=0&k=20&c=TthBCamXFsPZt2CaCfd2EzuzeImWiwKTUZJTMEIKFcI=')",
-      }}
-    >
-      <div className="bg-white bg-opacity-90 px-12 py-6 rounded-lg shadow-md relative sm:mx-4">
-        <div className="h-24 w-5/6 bg-cyan-950 absolute -top-4 left-1/2 transform -translate-x-1/2 rounded-lg flex items-center justify-center">
+      }}>
+      <div className="bg-white bg-opacity-90 px-10 py-2 rounded-lg shadow-md mx-2 items-center">
+        <div className="py-8 bg-cyan-950 rounded-lg flex items-center justify-center -translate-y-8">
           <p className="text-white font-mono font-semibold tracking-widest text-4xl">
             Vendure
           </p>
@@ -25,46 +24,45 @@ export default function SignInPage() {
             console.log(values);
           }}
         >
-          <Form className="w-full max-w-xl space-y-4 mt-24">
-            <div className="flex flex-col">
+          <Form className="w-full max-w-xl space-y-4 my-2">
+            <div className="mx-auto">
               <label htmlFor="username" className="text-gray-700 font-semibold mb-1">Username or Email:</label>
               <Field
                 type="text"
                 id="username"
                 name="username"
-                className="border border-gray-300 rounded-md py-2 px-6 w-full"
+                className="border border-gray-300 rounded-md py-2 px-6 w-full block"
               />
             </div>
-            <div className="flex flex-col pb-5">
+            <div className="pb-5 mx-auto">
               <label htmlFor="password" className="text-gray-700 font-semibold mb-1">Password:</label>
               <Field
                 type="password"
                 id="password"
                 name="password"
-                className="border border-gray-300 rounded-md py-2 px-6 w-full"
+                className="border border-gray-300 rounded-md py-2 px-6 w-full block"
               />
             </div>
-
 
             <Button
               type="primary"
               htmlType="submit"
-              className="w-full"
+              className="w-full block"
             >
               Sign In
             </Button>
 
             <div className="text-center pt-2">
-              <a href="#" className="text-sm text-amber-900">
+              <Link href="#" className="text-sm text-amber-900">
                 Forgot your Password?
-              </a>
+              </Link>
             </div>
             <div className="text-center">
               <p className="text-sm">
-                Don&apos;t have an account?{" "}
-                <a href="../sign-up" className="text-amber-500 font-semibold">
+                Don't have an account?{" "}
+                <Link href="../sign-up" className="text-amber-500 font-semibold">
                   Sign up
-                </a>
+                </Link>
               </p>
             </div>
           </Form>
